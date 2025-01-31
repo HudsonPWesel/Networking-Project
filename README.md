940489616aa2ad2f2add8d56bf15e50d8cb0633d
Hudson Pierce Wesel <cshudsonwesel@gmail.com>
2025-01-31 15:37:01 +0000

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+## This will be our design doc that outlines the project
---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Created Socket : " + location.host);
+const socket = new WebSocket("ws://"+location.host+"/");
+console.log("Created Socket" + location.host);
+
+socket.addEventListener("open", (event) => {
+  socket.send("Hello Server!");
+});
+
+socket.addEventListener("message", (event) => {
+  console.log("Message from server ", event.data);
+});

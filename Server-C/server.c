d8582bdf4de9c87eb54701fd13957e0ef9c06c0e
HudsonPWesel <cswyld7k@gmail.com>
2025-02-06 17:45:28 -0500

    Add Past Work

--- Server-C/server.c
+++ Server-C/server.c
@@ -6,15 +6,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <openssl/sha.h>
 
 #define PORT 4444
 #define LISTEN_BACKLOG 50
 #define KEY_NAME_LENGTH 19
 #define KEY_LENGTH 24
+#define GUID_LENGTH 36
+#define SHA1_DIGEST_LENGTH 20
 
 int main(int argc, char const *argv[]) {
 
     char buffer[1024];
+    char const SOCKET_GUID [37] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
     int server_fd, client_fd;
     socklen_t peer_addr_size;
@@ -45,8 +49,8 @@ int main(int argc, char const *argv[]) {
 
   // KEY_LENGTH + '\0'
 
-  char web_socket_key[KEY_LENGTH + 1];
-
+  char web_socket_key[KEY_LENGTH + GUID_LENGTH + 1];
+  char sha1_digest [SHA1_DIGEST_LENGTH + 1];
 
   for (;;){
 
@@ -54,11 +58,23 @@ int main(int argc, char const *argv[]) {
 
     if(client_fd){
       read(client_fd, buffer, sizeof(buffer));
-      if((memcpy(web_socket_key, strstr(buffer,"Sec-WebSocket-Key")+KEY_NAME_LENGTH,KEY_LENGTH)))
-        web_socket_key[KEY_LENGTH] = '\0';
-        printf("Full Header %s\n=======================", buffer);
-        printf("\n Web Socket Key %s\n=====================", web_socket_key);
+      if((memcpy(web_socket_key, strstr(buffer,"Sec-WebSocket-Key")+KEY_NAME_LENGTH,KEY_LENGTH))){
+        strcat(web_socket_key,SOCKET_GUID);
+        printf("%s\n", web_socket_key);
+
+        web_socket_key[KEY_LENGTH + GUID_LENGTH] = '\0';
+      }
+
+        //printf("Full Header %s\n=======================", buffer);
+        //printf("\n Web Socket Key %s\n=====================", web_socket_key);
+      
+
+      // Intiate handler respose
+      // == Crafting Sec-WebSocket-Accept == BASE64(SHA-1(KEY + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")) (this could be bac)
+      // Remember each header line ends with \r\n and put an extra \r\n after the last one to indicate the end of the header):
+      //
     }
+
 
   }
 

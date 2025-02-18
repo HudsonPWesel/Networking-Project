b40f78fe55dd942d546e32fbc2d430f7996f12db
HudsonPWesel <cswyld7k@gmail.com>
2025-02-17 22:59:02 -0500

    updated EchoServer

--- Server-C/IO-Multiplexing/EchoServer.c
+++ Server-C/IO-Multiplexing/EchoServer.c
@@ -22,91 +22,138 @@
 void str_echo(int sockfd);
 void sig_child(int signc);
 
-Socket();
+typedef struct ServerState {
+    int maxi;  // index into client[] array
+    int maxfd;
+    int client[FD_SETSIZE];
+    fd_set allset;
+    fd_set rset;
+    int listenfd;
+
+} ServerState;
+
+int Socket();
 void process_clients(int listenfd);
+void process_new_connection(ServerState *state);
 
 int Socket() {
     struct sockaddr_in serv_addr;
-
     int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        perror("socket error");
+        exit(1);
+    }
 
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(SERV_PORT);
 
-    bind(listenfd, (SA *)&serv_addr, sizeof(serv_addr));
-    listen(listenfd, LISTENQ);
+    if (bind(listenfd, (SA *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("bind error");
+        exit(3);
+    }
+
+    if (listen(listenfd, LISTENQ) < 0) {
+        perror("listen error");
+        exit(4);
+    }
 
     return listenfd;
 }
 
-void process_clients(int listenfd){
+void process_client_data(ServerState *state){
+  int sockfd, n;
+  char buff [MAXLINE];
 
-  int  maxi, maxfd, connfd, sockfd;
-  fd_set rset, allset;
-
-  maxfd = listenfd;
-  maxi = -1;
-
-  int nready, i, client[FD_SETSIZE];
-  size_t n;
-  socklen_t cli_len;
-
-  struct sockaddr_in cli_addr;
-  char buff[MAXLINE];
-
-  for (i = 0; i < FD_SETSIZE; i++) 
-    client[i] = -1;
-
-  FD_ZERO(&allset);
-  FD_SET(listenfd, &allset);
-
-  for (;;) {
-    rset = allset;
-    nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
-    if (FD_ISSET(listenfd, &rset)) {
-      cli_len = sizeof(cli_addr);
-      connfd = accept(listenfd, (SA *)&cli_addr, &cli_len);
-      i = 0;  // find an unused client to store the socket id
-      while (client[i] >= 0 && i < FD_SETSIZE) i++;
-      if (i < FD_SETSIZE) {
-        client[i] = connfd;
+  for (int i = 0; i <= state->maxi; i++) {
+    if ((sockfd = state->client[i]) < 0) continue;
+    if (FD_ISSET(sockfd, &(state->rset))) {
+      if ((n = read(sockfd, buff, MAXLINE)) == 0) {
+        close(sockfd);
+        FD_CLR(sockfd, &(state->allset));
+        state->client[i] = -1;
       } else {
-        printf("Too many clients!\n");
-        exit(1);
-      }
-      printf(" Client: %d fd: %d\n", i, connfd);
-      FD_SET(connfd, &allset);
-      if (connfd > maxfd) maxfd = connfd;
-      if (i > maxi) maxi = i;
-      if (--nready <= 0) continue;  // no more readable descriptors
-    }
-
-    for (i = 0; i <= maxi; i++) {
-      if ((sockfd = client[i]) < 0) continue;
-      if (FD_ISSET(sockfd, &rset)) {
-        if ((n = read(sockfd, buff, MAXLINE)) == 0) {
-          close(sockfd);
-          FD_CLR(sockfd, &allset);
-          client[i] = -1;
-        } else {
-          buff[n - 1] = '\0';
-          n--;
-          printf("READ %d: %s\n", sockfd, buff);
-          write(sockfd, buff, n);
-          printf("WRITE %d: %s\n", sockfd, buff);
-        }
-        if (--nready <= 0) break;
+        buff[n - 1] = '\0';
+        n--;
+        printf("READ %d: %s\n", sockfd, buff);
+        write(sockfd, buff, n);
+        printf("WRITE %d: %s\n", sockfd, buff);
       }
     }
   }
 }
 
-int main(int argc, char **argv) {
-    int listenfd = Socket();
-    
-    printf("The number of clients = %d\n", FD_SETSIZE);
+void process_new_connection(ServerState *state){
 
-    process_clients(listenfd);
+  struct sockaddr_in cli_addr;
+  socklen_t cli_len = sizeof(cli_addr);
+  int i = 0;
+
+  if (FD_ISSET(state->listenfd, &(state->rset))) {
+    int connfd = accept(state->listenfd, (SA *)&cli_addr, &cli_len);
+    if (connfd < 0) {
+        fprintf(stderr, "accept error\n");
+        return;
+    }
+
+    while (state->client[i] >= 0 && i < FD_SETSIZE) i++;
+
+    if (i < FD_SETSIZE) {
+      state->client[i] = connfd;
+    } else {
+      printf("Too many clients!\n");
+      exit(1);
+    }
+    printf(" Client: %d fd: %d\n", i, connfd);
+
+    FD_SET(connfd, &(state->allset));
+    if (connfd > state->maxfd) 
+            state->maxfd = connfd;
+        
+    if (i > state->maxi) 
+      state->maxi = i;
+    
+  }
+}
+
+void initialize_state(ServerState *state, int listenfd){
+
+  state->listenfd = listenfd;
+  state->maxfd = listenfd; 
+
+  state->maxi = -1;
+
+  for (int i = 0; i < FD_SETSIZE; i++) 
+    state->client[i] = -1;
+
+  FD_ZERO(&(state->allset));
+  FD_SET(listenfd, &(state->allset));
+
+}
+
+void process_clients(int listenfd){
+
+  ServerState state;
+  initialize_state(&state, listenfd);
+
+  for (;;) {
+    state.rset = state.allset;
+    int nready = select(state.maxfd + 1, &(state.rset), NULL, NULL, NULL);
+    if (nready < 0){
+      fprintf(stderr, "Select Error");
+      continue;
+    }
+
+    process_new_connection(&state);
+    process_client_data(&state);
+  }
+}
+
+int main(int argc, char **argv) {
+  int listenfd = Socket();
+
+  printf("The number of clients = %d\n", FD_SETSIZE);
+  process_clients(listenfd);
+  return 0;
 }
